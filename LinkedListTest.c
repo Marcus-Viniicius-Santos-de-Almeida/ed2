df1259b6e3dc5430fe1d80a99734cced3ef2095c
João Paulo Preti <jppreti@gmail.com>
2023-02-23 16:31:28 -0400

    Lista ligada

---
+++ LinkedListTest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "log.h"
+
+int main() {
+	FILE *file = fopen("program.log","w");
+	
+	log_set_level(LOG_INFO);
+	log_add_fp(file, LOG_TRACE);
+	
+	LinkedList lista;
+	init(&lista);
+
+	int *numero = malloc(sizeof(int));
+	*numero = 10;
+	enqueue(&lista, numero);
+	numero = malloc(sizeof(int));
+	*numero = 20;
+	enqueue(&lista, numero);
+    printf("%d\n", *((int*)dequeue(&lista)));
+    printf("%d\n", *((int*)dequeue(&lista)));
+    dequeue(&lista);
+				
+
+	return EXIT_SUCCESS;
+}
