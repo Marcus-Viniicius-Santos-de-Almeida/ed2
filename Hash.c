7d3ba9277bf64a99dfb3fb1c660863e927647e34
marcusvv <viniiciusm0@gmail.com>
2023-04-14 14:17:20 -0400

    aula 14/04/2023

--- Hash.c
+++ Hash.c
@@ -21,21 +21,38 @@ bool isHashEmpty(HashStruct *hashStruct){
 int hash(char *key){
     log_info("Calculando o hash para a chave");
     log_trace("hash ->");
-
+    int sum = 0;
+    for (int i = 0; key[i]!=0; i++){
+        sum+=key[i]*(i+1);
+    }
     log_trace("hash <-");    
-    return 1;}
+    return sum%MAX;
+}
 
 int put(HashStruct *hashStruct, char *key, void *data, compare equal){
-    
-    return 1;
+    log_info("Iserindo um par (chave, valor");
+    log_trace("put ->");
+    if (!containskay(hashStruct, key, equal)) {
+        int res = enqueue(&hashStruct->hashes[hash(key)], data);
+        hashStruct->size+=res;
+        log_debug("res: %d", res);
+        return res;
+    }
+    log_trace("hash <-");
+    return 0;
 }
 
 bool containsKey(HashStruct *hashStruct, char *key, compare equal){
-    
-    return NULL;}
+    log_info("Verificando se a chave já existe na tabela de hash");
+    log_trace("containskey ->");
+    int hashValue = hash(key);
+    int pos = indexOf(&hashStruct->hashes[hashValue], key, equal);
+    log_trace("containskay <-");
+    return (pos!=-1)?true:false;
+}
 
 void* get(HashStruct *hashStruct, char *key, compare equal){
-    
+     log_info("valor buscando pela chave");
     return NULL;}
 
 void* removeKey(HashStruct *hashStruct, char *key, compare equal){
