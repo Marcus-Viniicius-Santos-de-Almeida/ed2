8705f2df0f702109de1a7bed66cc1cae8d56b431
marcusvv <viniiciusm0@gmail.com>
2023-05-26 14:26:43 -0400

    Aula 26/05/2023

--- CompressTreeTest.c
+++ CompressTreeTest.c
@@ -7,6 +7,8 @@ typedef struct simbolo{
     int frequencia;
 }SIMBOLO;
 
+//void NewNode(SIMBOLO **edc, char *symbol, int frequency, )
+
 /*ordenarvetor{
 
 }
@@ -16,21 +18,25 @@ Agrupar{
 
 }*/
 
-int main(){
+int main(int argc, char* argv[]){
     int k;
     char ch, NomeArq[255];
-    SIMBOLO *symbols = (SIMBOLO *)malloc(sizeof(SIMBOLO));
-    printf("Digite o nome do arquivo: ");
-    scanf("%s", &NomeArq[k]);
-    FILE *Arquivo = fopen(NomeArq, "r");
+    SIMBOLO *symbols = (SIMBOLO *)malloc(sizeof(SIMBOLO)*200);
+    //printf("Digite o nome do arquivo: ");
+    //scanf("%s", &NomeArq[k]);
+    FILE *Arquivo = fopen(argv[1], "r");
     int i = 0;
-    do{
-        symbols[i].symbol = fgetc(Arquivo);
-    }while(ch=fgetc(Arquivo)!=EOF);
+    while(!feof(Arquivo)){
+         symbols[i].symbol = fgetc(Arquivo);
+         printf("%c ", symbols[i].symbol);
+         i++;
+    }
+        
+    
 
-    for(int j=0; j<=i; j++){
+    /*for(int j=0; j<=i; j++){
         printf("%c ", symbols[j].symbol);
     }
-    fclose(Arquivo);
+    fclose(Arquivo);*/
     return 0;
 }
