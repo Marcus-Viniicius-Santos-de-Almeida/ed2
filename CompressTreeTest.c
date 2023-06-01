d0ada95f937f674ffc7ccf372bcb18cbb0af0096
Marcus-Viniicius-Santos-de-Almeida <viniiciusm0@gmail.com>
2023-06-01 15:11:11 -0400

    01/06/2023

--- CompressTreeTest.c
+++ CompressTreeTest.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "log.h"
 
-typedef struct simbolo{
-    char symbol;
-    int frequencia;
-}SIMBOLO;
+
 
 //void NewNode(SIMBOLO **edc, char *symbol, int frequency, )
 
@@ -18,18 +16,37 @@ Agrupar{
 
 }*/
 
+typedef struct simbolo{
+    char symbol;
+    int frequencia;
+    struct simbolo *left;
+    struct simbolo *right;
+}SIMBOLO;
+
 int main(int argc, char* argv[]){
-    int k;
-    char ch, NomeArq[255];
-    SIMBOLO *symbols = (SIMBOLO *)malloc(sizeof(SIMBOLO)*200);
+    int k, c;
+    int t = 0;
+    char ch, NomeArq[255], SC;
+    SIMBOLO *symbols = (SIMBOLO *)malloc(sizeof(SIMBOLO)*99999999);
     //printf("Digite o nome do arquivo: ");
     //scanf("%s", &NomeArq[k]);
     FILE *Arquivo = fopen(argv[1], "r");
     int i = 0;
     while(!feof(Arquivo)){
-         symbols[i].symbol = fgetc(Arquivo);
-         printf("%c ", symbols[i].symbol);
-         i++;
+        c = 0;
+        SC = '\0';
+        SC = fgetc(Arquivo);
+        while(symbols[t]!=NULL){
+            if(symbols[t].symbol==SC){
+            c++;
+            t++;
+            }
+        }
+        if (c==0){
+            symbols[i].symbol = SC;
+        }
+        printf("%c ", symbols[i].symbol);
+        i++;
     }
         
     
